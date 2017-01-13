f5a176ae61f38f4c2f9f4c5d7cb0dfe43f7b1d40
康育哲 <kangyuzhe@zeta-inc.co.jp>
2017-01-13 10:23:03 +0900

    Homework for Lesson 2

--- src/com/zeta/ex02/Homework.java
+++
@@ -1,7 +0,0 @@
-package com.zeta.ex02;
-
-/**
- * Created by kang on 07/01/2017.
- */
-public class Homework {
-}
---
+++ src/com/zeta/ex02/Student.java
@@ -0,0 +1,8 @@
+package com.zeta.ex02;
+
+/**
+ * 作业题：完成第3讲讲义的最后一道练习题
+ */
+public class Student {
+
+}
